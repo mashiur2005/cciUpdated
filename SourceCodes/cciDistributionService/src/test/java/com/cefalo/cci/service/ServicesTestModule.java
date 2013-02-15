42a3fdafb5bff797778135ab04d51585d138d56a
mashiur <mashiur2005@gmail.com>
2013-02-15 08:18:00 +0600

    CCIDIST-1: Unnecessary comments removed from the code

--- SourceCodes/cciDistributionService/src/test/java/com/cefalo/cci/service/ServicesTestModule.java
+++ SourceCodes/cciDistributionService/src/test/java/com/cefalo/cci/service/ServicesTestModule.java
@@ -1,19 +1,8 @@
-package com.cefalo.cci.service;// TestModule.java, created by Fabio Strozzi on Mar 27, 2011
+package com.cefalo.cci.service;
 
 import com.google.inject.AbstractModule;
 
-/**
- * A trivial Guice configuration.
- * 
- * @author Fabio Strozzi
- */
 public class ServicesTestModule extends AbstractModule {
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.google.inject.AbstractModule#configure()
-     */
     @Override
     protected void configure() {
       bind(HelloService.class).to(HelloServiceImpl.class);
